1727764296c93c3aef6fb893e6e165ad3e834c2d
Luan Argolo <ipsluan@icloud.com>
2022-11-13 12:12:04 -0300

    Exercícios

--- script.ts
+++ script.ts
@@ -1,52 +1,18 @@
-// function normalizarTexto(texto) {
-//   return texto.trims().toLowercase();
-// }
+// Exercício
+// 1 - Crie uma função chamada toNumber
+// 2 - A função pode receber number | string
+// 3 - Se a função receber um número, retorne um número
+// 4 - Se a função receber uma string, retorne um número
+// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
 
-function normalizarTexto(texto: string) {
-  return texto.trim().toLowerCase();
-}
-
-console.log(normalizarTexto("DEsigN"));
-
-// const input = document.querySelector("input");
-// const total = localStorage.getItem("total");
-
-// input.value = total;
-// calcularGanho(input.value);
-
-// function calcularGanho(value) {
-//   const p = document.querySelector("p");
-//   p.innerText = `Ganho total: ${value + 100 - value * 0.2}`;
-// }
-
-// function totalMudou() {
-//   const value = Number(input.value);
-//   localStorage.setItem("total", value);
-//   calcularGanho(value);
-// }
-
-// input.addEventListener("keyup", totalMudou);
-
-const input = document.querySelector("input");
-const total = localStorage.getItem("total");
-if (input && total) {
-  input.value = total;
-  calcularGanho(Number(input.value));
-}
-
-function calcularGanho(value: number) {
-  const p = document.querySelector("p");
-  if (p) {
-    p.innerText = `Ganho total: ${value + 100 - value * 0.2}`;
+function toNumber(number: number | string) {
+  if (typeof number === "number") {
+    return number;
+  } else if (typeof number === "string") {
+    return Number(number);
+  } else {
+    throw "value deve ser um number | string";
   }
 }
 
-function totalMudou() {
-  if (input) {
-    const value = Number(input.value);
-    localStorage.setItem("total", String(value));
-    calcularGanho(value);
-  }
-}
-
-input?.addEventListener("keyup", totalMudou);
+console.log(Number(true));
