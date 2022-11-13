17da11a8d465305983b37353d187aafaf8c77275
Luan Argolo <ipsluan@icloud.com>
2022-11-13 00:37:57 -0300

    Types

---
+++ script.js
@@ -0,0 +1,20 @@
+//@ts-check
+const frase = "Front End";
+const total = 100.05;
+const empresas = ["Apple", "Microsoft"];
+
+frase.toLowerCase();
+
+const t = total.toFixed();
+console.log(t);
+
+empresas.map((empresa) => empresa.toLowerCase());
+
+const body = document.body;
+body.style.background = "#000";
+
+const button = document.querySelector("button");
+// button.click();
+
+const operacao = 100 + {};
+console.log(operacao);
