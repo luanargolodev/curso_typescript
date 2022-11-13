aec1ff03e4bb4d2350f6574bacee69ec3dba2903
Luan Argolo <ipsluan@icloud.com>
2022-11-13 13:14:08 -0300

    Exercício - Interface

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 "use strict";
-// Exercício
-// 1 - Crie uma função chamada toNumber
-// 2 - A função pode receber number | string
-// 3 - Se a função receber um número, retorne um número
-// 4 - Se a função receber uma string, retorne um número
-// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
-function toNumber(number) {
-    if (typeof number === "number") {
-        return number;
-    }
-    else if (typeof number === "string") {
-        return Number(number);
-    }
-    else {
-        throw "value deve ser um number | string";
+// Type e Interface
+// Type - A palavra chave type cria um atalho (alias) para um tipo customizado.
+// Interface - Funciona na maioria dos casos da mesma forma que type, porém possui uma sintaxe diferente. As interfaces são geralmente utilizadas para definirmos objetos.
+function preencherDados(dados) {
+    document.body.innerHTML += `
+    <div>
+      <h2>${dados.nome}</h2>
+      <p>R$ ${dados.preco}</p>
+      <p>Inclui teclado: ${dados.teclado ? "sim" : "não"}</p>
+    </div>
+  `;
+}
+const computador = {
+    nome: "Computador",
+    preco: 5500,
+    teclado: true,
+};
+preencherDados(computador);
+preencherDados({
+    nome: "Notebook",
+    preco: 2500,
+    teclado: false,
+});
+function pintarCategoria(categoria) {
+    console.log(categoria);
+    if (categoria === "design") {
+        console.log("Pintar vermelho");
     }
 }
-console.log(Number(true));
+pintarCategoria("design");
+async function fetchProduct() {
+    const response = await fetch("https://api.origamid.dev/json/notebook.json");
+    const data = await response.json();
+    console.log(data);
+    showProduct(data);
+}
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <div>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      </div>
+      <div>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      </div>
+    </div>
+  `;
+}
--- script.ts
+++ script.ts
@@ -1,18 +1,85 @@
-// Exercício
-// 1 - Crie uma função chamada toNumber
-// 2 - A função pode receber number | string
-// 3 - Se a função receber um número, retorne um número
-// 4 - Se a função receber uma string, retorne um número
-// 5 - Se ela receber algo diferente, retorne um erro. (throw "value deve ser um número ou uma string")
+// Type e Interface
+// Type - A palavra chave type cria um atalho (alias) para um tipo customizado.
+// Interface - Funciona na maioria dos casos da mesma forma que type, porém possui uma sintaxe diferente. As interfaces são geralmente utilizadas para definirmos objetos.
 
-function toNumber(number: number | string) {
-  if (typeof number === "number") {
-    return number;
-  } else if (typeof number === "string") {
-    return Number(number);
-  } else {
-    throw "value deve ser um number | string";
+interface Produto {
+  nome: string;
+  preco: number;
+  teclado: boolean;
+}
+
+function preencherDados(dados: Produto) {
+  document.body.innerHTML += `
+    <div>
+      <h2>${dados.nome}</h2>
+      <p>R$ ${dados.preco}</p>
+      <p>Inclui teclado: ${dados.teclado ? "sim" : "não"}</p>
+    </div>
+  `;
+}
+
+const computador: Produto = {
+  nome: "Computador",
+  preco: 5500,
+  teclado: true,
+};
+
+preencherDados(computador);
+
+preencherDados({
+  nome: "Notebook",
+  preco: 2500,
+  teclado: false,
+});
+
+type Categorias = "design" | "código" | "descod";
+
+function pintarCategoria(categoria: Categorias) {
+  console.log(categoria);
+  if (categoria === "design") {
+    console.log("Pintar vermelho");
   }
 }
 
-console.log(Number(true));
+pintarCategoria("design");
+
+// Exercício - Defina a interface da API: https://api.origamid.dev/json/notebook.json e mostre os dados na tela
+interface Empresa {
+  nome: string;
+  fundacao: number;
+  pais: string;
+}
+
+interface ProdutoApi {
+  nome: string;
+  preco: number;
+  descricao: string;
+  garantia: string;
+  seguroAcidentes: boolean;
+  empresaFabricante: Empresa;
+  empresaMontadora: Empresa;
+}
+
+async function fetchProduct() {
+  const response = await fetch("https://api.origamid.dev/json/notebook.json");
+  const data = await response.json();
+  console.log(data);
+  showProduct(data);
+}
+
+fetchProduct();
+
+function showProduct(data: ProdutoApi) {
+  document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <p>R$ ${data.preco}</p>
+      <div>
+        <h3>Fabricante: ${data.empresaFabricante.nome}</h3>
+      </div>
+      <div>
+        <h3>Montadora: ${data.empresaMontadora.nome}</h3>
+      </div>
+    </div>
+  `;
+}
