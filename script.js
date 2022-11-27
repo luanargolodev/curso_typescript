6a4eb49f60ddb20582e3b6f3599db2b6bdf5c4d4
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2022-11-27 13:57:07 -0300

    Exercício resolvido Instanceof

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 // Exercício
 // Selecione o link utilizando o método getElementsById
 // Substitua o href do link (HTMLAnchorElement) de http:// para https://
+const $link = document.getElementById('origamid');
+if ($link instanceof HTMLAnchorElement) {
+    $link.href = $link.href.replace('http://', 'https://');
+}
